7f6eb87cba8548a698ce1f05f7941d70809d354a
ぐらむ/gramme <127509194+gramme-linkcom@users.noreply.github.com>
2024-05-04 23:47:32 +0900

    Add files via upload

--- link.js
+++ link.js
@@ -2,19 +2,19 @@ window.onload = onLoad;
 
 function onLoad() {
     let link = document.getElementById('news');
-    link.setAttribute('href', "/linkcom/news/index.html");
+    link.setAttribute('href', "news/index.html");
 
     link = document.getElementById('about');
-    link.setAttribute('href', "/linkcom/about/index.html");
+    link.setAttribute('href', "about/index.html");
     
     link = document.getElementById('ir');
-    link.setAttribute('href', "/linkcom/ir/index.html");
+    link.setAttribute('href', "ir/index.html");
     
     link = document.getElementById('recruit');
-    link.setAttribute('href', "/linkcom/recruit/index.html");
+    link.setAttribute('href', "recruit/index.html");
 
     link = document.getElementById('base');
-    let root = "gramme-linkcom.github.io"
+    let root = "csmc.linkcom.f5.si"
     link.setAttribute('href', "http://"+root);
 
     console.log("completed rewrite")
